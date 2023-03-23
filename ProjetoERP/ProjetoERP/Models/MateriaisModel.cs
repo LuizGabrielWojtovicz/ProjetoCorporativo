9d679246f4bedacc266eac353e68e31f141b6c6d
Lugaw <83958914+LuizGabrielWojtovicz@users.noreply.github.com>
2023-03-23 06:34:07 -0300

    Add files via upload

---
+++ ProjetoERP/ProjetoERP/Models/MateriaisModel.cs
@@ -0,0 +1,38 @@
+﻿
+/*
+*MT <=> MATERIAIS
+*PD <=> PRODUTOS
+*VD <=> VENDAS
+*CT <=> CLIENTES
+*FN <=> FUNCIONARIOS
+*PV <=> PRODUTOS VENDIDOS
+*/
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoErp.Models
+{
+    public enum Importancia_MT { ALTA, MEDIA, BAIXA}
+    public enum Status_MT { ATIVO, INATIVO, ABSOLETO}
+    public class MateriaisModel
+    {
+        //Primary Key MateriaisModel
+        [Key]
+        public int id_MT { get; set; }
+
+        //Attributes
+        public string? tipo_MT { get; set; }
+        public int quantidadeEstoque_MT { get; set; }
+        public string? unidadeMedida_MT { get; set; }
+        public int quantidadeEstoqueMinimo_MT { get; set; }
+        public int quantidadeEstoqueMaximo_MT { get; set; }
+        public int tempoEntregaDia_MT { get; set; }
+        public DateTime dataUltimaEntrega_MT { get; set; }
+        public string? fornecedor_MT { get; set; }
+        public double custoPadrao_MT { get; set; }
+        public Importancia_MT importancia_MT { get;set; }
+        public Status_MT status_MT { get; set; }
+        public string? especificacoesTecnicas_MT { get; set; }
+        public string? descricao_MT { get; set; }
+    }
+}
---
+++ ProjetoERP/ProjetoERP/Models/VendasModel.cs
@@ -0,0 +1,44 @@
+﻿
+/*
+*MT <=> MATERIAIS
+*PD <=> PRODUTOS
+*VD <=> VENDAS
+*CT <=> CLIENTES
+*FN <=> FUNCIONARIOS
+*PV <=> PRODUTOS VENDIDOS
+*/
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoErp.Models
+{
+    public enum MetodoPagamento_VD { DINHEIRO, CREDITO, DEBITO, PIX}
+    public enum Status_VD { ABERTO, CANCELADO, CONCLUIDO}
+    public enum TipoVenda_VD { A_VISTA, A_PRAZO}
+    public class VendasModel
+    {
+        //Primary Key VendasModel
+        [Key]
+        public int id_VD { get; set; }
+
+        //Foreign Key ClientesModel
+        [ForeignKey("ClientesModel")]
+        public int id_CT { get; set; }
+
+        //Foreign Key FuncionariosModel
+        [ForeignKey("FuncionariosModel")]
+        public int id_FN { get; set; }
+
+        //Attributes
+        public List<ProdutosVendidosModel>? produtosVendidos { get; set; }
+        public DateTime data_VD { get; set; }
+        public DateTime dataPagamento_VD { get; set; }
+        public Status_VD status_VD { get; set; }
+        public double valorTotal_VD { get; set; }
+        public double desconto_VD { get; set; }
+        public MetodoPagamento_VD metodoPagamento_VD { get; set; }
+        public TipoVenda_VD tipoVenda_VD { get; set; }
+        public string? descricao_VD { get; set; }
+    }
+}
